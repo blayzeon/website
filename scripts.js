c49843e7e1c4ef94203db408b7e23974bc9f9698
blayzeon <blayzeon@gmail.com>
2023-05-29 16:16:42 -0400

    format with prettier

--- scripts.js
+++ scripts.js
@@ -1,40 +1,40 @@
 const defaultSetting = {
-    parent: "container",
-    img: "https://images.pexels.com/photos/15985573/pexels-photo-15985573/free-photo-of-wood-landscape-water-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    caption: "Test",
-    text: "Subtext for the text.",
-  };
-  
-  const settings = [{ ...defaultSetting }];
-  const elms = {
-    card: (object) => {
-      /* safety net */
-      const safety = { ...defaultSetting };
-      const obj = { ...safety, ...object };
+  parent: "container",
+  img: "https://images.pexels.com/photos/15985573/pexels-photo-15985573/free-photo-of-wood-landscape-water-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  caption: "Test",
+  text: "Subtext for the text.",
+};
 
-      /* elements */
-      const parent = document.getElementById(obj.parent);
-      const elm = document.createElement("div");
-      const caption = document.createElement("h2");
-      const text = document.createElement("p");
+const settings = [{ ...defaultSetting }];
+const elms = {
+  card: (object) => {
+    /* safety net */
+    const safety = { ...defaultSetting };
+    const obj = { ...safety, ...object };
 
-      /* classes */
-      elm.classList.add("card");
-      caption.classList.add("title");
-      text.classList.add("subtext");
+    /* elements */
+    const parent = document.getElementById(obj.parent);
+    const elm = document.createElement("div");
+    const caption = document.createElement("h2");
+    const text = document.createElement("p");
 
-      /* modifications */
-      elm.style.backgroundImage = `url(${obj.img})`;
-      caption.innerText = obj.caption;
-      text.innerText = obj.text;
+    /* classes */
+    elm.classList.add("card");
+    caption.classList.add("title");
+    text.classList.add("subtext");
 
-      /* add & return */
-      parent.appendChild(elm);
-      elm.appendChild(caption);
-      elm.appendChild(text);
+    /* modifications */
+    elm.style.backgroundImage = `url(${obj.img})`;
+    caption.innerText = obj.caption;
+    text.innerText = obj.text;
 
-      return elm;
-    },
-  };
+    /* add & return */
+    parent.appendChild(elm);
+    elm.appendChild(caption);
+    elm.appendChild(text);
 
-  settings.forEach((setting) => elms.card(setting));
+    return elm;
+  },
+};
+
+settings.forEach((setting) => elms.card(setting));
